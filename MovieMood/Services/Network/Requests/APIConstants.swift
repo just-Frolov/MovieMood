8984ea1911d06e6fdcfe4c21bf18f3c6aca2642a
Danil <frolov.danil.natife@gmail.com>
2023-07-16 17:53:14 +0300

    added ApiConstants file

---
+++ MovieMood/Services/Network/Requests/APIConstants.swift
@@ -0,0 +1,16 @@
+//
+//  APIConstants.swift
+//  MovieMood
+//
+//  Created by Danil Frolov on 16.07.2023.
+//
+
+import Foundation
+
+struct APIConstants {
+    struct MovieListFields {
+        static let path = "discover/movie"
+        static let page = "page"
+        static let sortType = "sort_by"
+    }
+}
